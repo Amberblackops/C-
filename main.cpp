5e1c78fb118532ab84a0885d23fac2d2095554a6
Amber bharti <amberb034@gmail.com>
2021-03-21 16:28:59 +0530

    New

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marksInMaths=83;
+    cout<<"The marks of the student in maths is  "<<marksInMaths;
+
+    short a;
+    int b;
+    long c;
+    long long d;
+
+    return 0;
+}  
